716a5ee9ed306b6f9e4d5bac09d5d7eb8a35ace7
neyser <neyser@digitechfp.com>
2023-12-18 10:14:45 +0100

    agregando segunda pantalla

--- Views/Shared/Menu1View.php
+++ Views/Shared/Menu1View.php
@@ -1,4 +1,5 @@
 <div class="menu1">
     <a href="index.php">Inicio</a>
     <a href="insercion-con-declaraciones-preparadas1.php">Insertar Contenido</a>
+    <a href="insercion-con-declaraciones-preparadas2.php">Insertar Contenido y artistas</a>
 </div>
